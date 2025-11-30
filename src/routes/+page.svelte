1b5bfc9607258f5f6d1923694cad93b1354ac79f
u5er1ame <lapd.ga@gmail.com>
2025-11-30 16:45:51 +0300

    batman

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="text-center">
+	<form method="POST" action="/api/rooms">
+		<input class="form-input" type="text" name="name" placeholder="Room name" />
+		<input class="form-input" type="number" step=".01" name="area" placeholder="Area" />
+		<button type="submit">Create room</button>
+	</form>
+</div>
