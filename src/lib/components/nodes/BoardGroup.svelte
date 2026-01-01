90bd835adc1d002544372418ae52a46ce4d4e29d
u5er1ame <lapd.ga@gmail.com>
2026-01-01 21:43:22 +0300

    restyle room and make resize limit

--- src/lib/components/nodes/BoardGroup.svelte
+++ src/lib/components/nodes/BoardGroup.svelte
@@ -1,77 +1,95 @@
 <script lang="ts">
-    import { fade } from 'svelte/transition';
-    import { ControlButton, NodeResizer, NodeToolbar, Position, type Node, type NodeProps } from '@xyflow/svelte';
-    import { useOnSelectionChange } from '@xyflow/svelte';
-    import { RecordId } from 'surrealdb';
-    import { twMerge } from 'tailwind-merge';
-    import CustomControlButton from '../CustomControlButton.svelte';
-    import type { Board } from '$lib/server/schemas';
+import { fade } from 'svelte/transition';
+import { useResizeObserver } from "runed";
+import { ControlButton, NodeResizer, NodeToolbar, Position, type Node, type NodeProps } from '@xyflow/svelte';
+import { useOnSelectionChange } from '@xyflow/svelte';
+import type { Board } from '$lib/server/schemas';
 
 
-    type Props = {
-	data?: Board,
-	class?: string
-    } & NodeProps<Node<Board>>
+type Props = {
+    data?: Board,
+    class?: string
+} & NodeProps<Node<Board>>
 
-    let selected = $state(false);
-    let resizeable = $state(false);
+let node: HTMLDivElement | undefined = $state();
 
-    useOnSelectionChange(({nodes})=>{
-	if (nodes.some(n=>n.id == id)) {
-	    selected = true;
-	}
-	else {
-	    selected = false;
-	    editTitle = false;
-	}
-    });
+let resizeProps = $state({
+    minWidth: 16,
+    minHeight: 16,
+    maxWidth: 128, // TODO: bind to parent
+    maxHeight: 128,
+});
 
-    let { id, data, class: className, ...rest }: Props = $props();
-    id = id || data?.id.toString();
-    let editTitle = $state(false);
-    let name = $state(data?.name);
+const clamp = (value: number, min: number, max: number) =>
+  Math.min(Math.max(value, min), max);
 
-    const resizebuttonColor = $derived.by(()=>{
-	if (resizeable) {
-	    return "bg-yellow-100 hover:bg-yellow-200";
-	}
-	return "bg-slate-100 hover:bg-sky-200";
-    });
-    const onfocus = (e: FocusEvent &{ currentTarget: EventTarget & HTMLInputElement})=>e.currentTarget.select()
-    // TODO: validate data by schema here or on fetch?
+useResizeObserver(()=>node, ([info])=>{
+    if (!node || !info) return;
+    resizeProps.minWidth = clamp(info.contentRect.width, node.scrollWidth , resizeProps.maxWidth)+8;
+    resizeProps.minHeight = clamp(info.contentRect.height, node.scrollHeight, resizeProps.maxHeight)+8;
+});
+
+
+let { id, data, class: className, ...rest }: Props = $props();
+id = id || data?.id.toString();
+
+let selectedNodes = $state<string[]>([]);
+
+useOnSelectionChange(({nodes})=>{
+    const selection = nodes.filter(n=>n.selected);
+    selectedNodes = selection.map(n=>n.id);
+});
+
+let selected = $derived.by(()=>selectedNodes.length > 0 && selectedNodes.every(item=>item == id));
+let resizeable = $state(false);
+
+const resizeControlProps = $derived.by(()=>{
+    if (resizeable) {
+	return {
+	    title: "Disable resizing",
+	    color: "var(--color-orange-500)",
+	    bgColorHover: "var(--color-orange-500)",
+	    borderColor: "var(--color-orange-500)",
+	};
+    }
+    return {
+	title: "Enable resizing",
+    };
+});
+const onfocus = (e: FocusEvent &{ currentTarget: EventTarget & HTMLInputElement})=>e.currentTarget.select()
+// TODO: validate data by schema here or on fetch?
 </script>
 
-<div class="size-full flex items-stretch">
-    <p class="size-full text-amber-300 text-[10px]">{data?.name}</p>
+<div bind:this={node} class="size-full flex items-stretch">
+    <p class="size-full text-stone-300 text-[10px]">{data?.name}</p>
+    <NodeResizer {...resizeProps} handleClass="hidden invisible" lineClass="rounded-lg" isVisible={selected && resizeable} color="var(--color-yellow-100)" class="rounded-lg" nodeId={id} />
+    <NodeToolbar class="text-slate-500 h-full"  position={Position.Right} align="start" nodeId={id}>
+	<div class="flex flex-col gap-1 *:rounded-lg" transition:fade>
+	    <ControlButton   title="Add board" onclick={()=>console.log("click")}>
+		<span class="icon-[material-symbols--add-2-rounded]"></span>
+	    </ControlButton>
+	    <ControlButton {...resizeControlProps} onclick={()=>resizeable=!resizeable}>
+		<span class="icon-[material-symbols--resize-rounded]"></span>
+	    </ControlButton>
+	</div>
+    </NodeToolbar>
+    <NodeToolbar class="text-slate-500" offset={-4}  position={Position.Top} align="center" nodeId={id}>
+    </NodeToolbar>
+    <NodeToolbar class="text-slate-500" offset={-5}  position={Position.Bottom} align="start" nodeId={id}>
+    </NodeToolbar>
 </div>
-<NodeResizer isVisible={selected && resizeable} color="var(--color-yellow-100)" class="rounded-lg" nodeId={id} />
-<NodeToolbar class="text-slate-500 h-full"  position={Position.Right} align="start" nodeId={id}>
-    <div class="flex flex-col gap-1 *:rounded-lg" transition:fade>
-	<ControlButton   title="Add board" onclick={()=>console.log("click")}>
-	    <span class="icon-[material-symbols--add-2-rounded]"></span>
-	</ControlButton>
-	<CustomControlButton type="button" class={twMerge(resizebuttonColor)} title="Add board" onclick={()=>resizeable=!resizeable}>
-	    <span class="icon-[material-symbols--resize-rounded]"></span>
-	</CustomControlButton>
-    </div>
-</NodeToolbar>
-<NodeToolbar class="text-slate-500" offset={-4}  position={Position.Top} align="center" nodeId={id}>
-</NodeToolbar>
-<NodeToolbar class="text-slate-500" offset={-5}  position={Position.Bottom} align="start" nodeId={id}>
-</NodeToolbar>
 
 <style>
 :global(.svelte-flow__node-boards.selectable) {
-    padding: 4px;
-    border-radius: 0px;
-    width: "auto";
-    color: var(--xy-node-color, var(--xy-node-color-default));
-    text-align: center;
-    border: var(--xy-node-border, var(--xy-node-border-default));
-    background-color: var(--color-slate-100, var(--xy-node-background-color-default));
+padding: 4px;
+border-radius: 2px;
+width: "auto";
+color: var(--color-stone-200, var(--xy-node-color-default));
+text-align: center;
+border: var(--xy-node-border, var(--xy-node-border-default));
 }
+
 :global(.svelte-flow__node-boards.selectable:hover) {
-    box-shadow: var(--shadow-lg);
-    border-color: var(--color-emerald-100, var(--xy-node-border-color-default));
+box-shadow: var(--shadow-lg);
 }
 </style>
--- src/lib/components/nodes/RoomGroup.svelte
+++ src/lib/components/nodes/RoomGroup.svelte
@@ -2,11 +2,9 @@
     import { fade } from 'svelte/transition';
     import { ControlButton, NodeResizer, NodeToolbar, Position, type Node, type NodeProps } from '@xyflow/svelte';
     import { useOnSelectionChange, useViewport, useSvelteFlow, useInternalNode } from '@xyflow/svelte';
-    import { RecordId } from 'surrealdb';
     import { twMerge } from 'tailwind-merge';
     import CustomControlButton from '../CustomControlButton.svelte';
 
-    import { getBoards } from "../../../routes/room.remote";
     import type {  Board, ElectricRoom } from '$lib/server/schemas';
 
     type Props = {
@@ -14,27 +12,22 @@
 	class?: string
 	} & NodeProps<Node<ElectricRoom>>
 
-    let selected = $state(false);
-    let resizeable = $state(false);
+    let selectedNodes = $state<string[]>([]);
 
     useOnSelectionChange(({nodes})=>{
-	if (nodes.some(n=>n.id == id)) {
-	    selected = true;
-	}
-	else {
-	    selected = false;
-	    editTitle = false;
-	}
+	const selection = nodes.filter(n=>n.selected);
+	selectedNodes = selection.map(n=>n.id);
     });
+
+    let selected = $derived.by(()=>selectedNodes.length > 0 && selectedNodes.every(item=>item == id));
+    let resizeable = $state(false);
 
     let { id, data, class: className, ...rest }: Props = $props();
 
     id = id || data?.id.toString();
 
-    const boards: Board[] = $derived(await getBoards(data.boards));
 
-    let editTitle = $state(false);
-
+    let editName = $state(false);
     let name = $state(data?.name);
 
     const viewport = useViewport();
@@ -47,19 +40,28 @@
 	flow.fitBounds(flow.getNodesBounds([node.current!]),{ padding: .1 });
     }
 
-    const resizebuttonColor = $derived.by(()=>{
+    const resizeControlProps = $derived.by(()=>{
 	if (resizeable) {
-	    return "bg-yellow-100 hover:bg-yellow-200";
+	    return {
+		title: "Disable resizing",
+		color: "var(--color-orange-500)",
+		bgColorHover: "var(--color-orange-500)",
+		borderColor: "var(--color-orange-500)",
+	    };
 	}
-	return "bg-slate-100 hover:bg-sky-200";
+	return {
+	    title: "Enable resizing",
+	};
     });
+
     let zoom = $derived(viewport.current.zoom > 1);
 
     const onfocus = (e: FocusEvent &{ currentTarget: EventTarget & HTMLInputElement})=>e.currentTarget.select()
     // isConnectable = false; // always false for groups
     // TODO: validate data by schema here or on fetch?
 </script>
-<div {ondblclick} transition:fade class="size-full" role="list">
+<div {ondblclick} transition:fade class="room size-full" role="list">
+    <NodeResizer isVisible={selected && resizeable}  class="rounded-lg" nodeId={id} />
     {#if !zoom}
 	<NodeToolbar class="text-slate-500" offset={0}  position={Position.Top} align="start" nodeId={id}>
 	    <CustomControlButton type="button" class="hover:text-orange-400" title="Edit board" onclick={ondblclick}>
@@ -70,25 +72,22 @@
 	    <p class="font-bold text-5xl text-slate-400">{name}</p>
 	</div>
     {:else}
-	{#each boards as b}
-	    <h1>{b.name}</h1>
-	{/each}
-	<NodeResizer isVisible={selected && resizeable} color="var(--color-yellow-100)" class="rounded-lg" nodeId={id} />
 	<NodeToolbar class="text-slate-500 h-full"  position={Position.Right} align="start" nodeId={id}>
 	    <div class="flex flex-col gap-1 *:rounded-lg" transition:fade>
-		<ControlButton  title="Add board" onclick={()=>console.log("click")}>
+		<ControlButton title="Add board" onclick={()=>console.log("click")}>
 		    <span class="icon-[material-symbols--add-2-rounded]"></span>
 		</ControlButton>
-		<CustomControlButton type="button" class={twMerge(resizebuttonColor)} title="Add board" onclick={()=>resizeable=!resizeable}>
+		<ControlButton {...resizeControlProps} type="button" onclick={()=>resizeable=!resizeable}>
 		    <span class="icon-[material-symbols--resize-rounded]"></span>
-		</CustomControlButton>
+		</ControlButton>
 	    </div>
 	</NodeToolbar>
 	<NodeToolbar class="text-slate-500" offset={-4}  position={Position.Top} align="center" nodeId={id}>
-	    {#if editTitle}
-		<input autofocus {onfocus} class="w-full text-lg focus:bg-yellow-50 text-slate-500 bg-transparent" bind:value={name} onblur={()=>editTitle=false}>
+	    {#if editName}
+		<!-- svelte-ignore a11y_autofocus -->
+		<input autofocus {onfocus} class="w-full text-lg focus:bg-yellow-50 text-slate-500 bg-transparent" bind:value={name} onblur={()=>editName=false}>
 	    {:else}
-		<p ondblclick={()=>{editTitle=true}} class="font-bold text-lg text-slate-500">{name}</p>
+		<p ondblclick={()=>{editName=true}} class="font-bold text-lg text-slate-500">{name}</p>
 	    {/if}
 	</NodeToolbar>
 	<NodeToolbar class="text-slate-500" offset={-5}  position={Position.Bottom} align="start" nodeId={id}>
@@ -97,23 +96,22 @@
     {/if}
 </div>
 <style>
-:global(.svelte-flow__node-electric_rooms.selectable) {
-    padding: 10px;
-    padding-bottom: 0px;
-    border-radius: 8px;
-    width: 150px;
+.room {
     font-size: 10px;
     color: var(--xy-node-group-background-color-default, var(--xy-node-group-background-color-default));
-    text-align: center;
-    border: var(--xy-node-border, var(--xy-node-border-default));
-    background-color: var(--xy-node-group-background-color, var(--xy-node-group-background-color-default));
-    backdrop-filter: blur(1px);
+    text-align: left;
 }
-:global(.svelte-flow__node-electric_rooms.selectable:hover) {
-    box-shadow: var(--shadow-lg);
+:global(.svelte-flow__node-electric_rooms.selectable) {
+    padding: 10px;
+    font-size: 10px;
+    background-color: var(--xy-node-group-background-color-default, var(--xy-node-group-background-color-default));
+    text-align: center;
+    border: 1px dashed --alpha(var(--color-stone-500)/30%);
+    backdrop-filter: blur(2px);
 }
 :global(.svelte-flow__node-electric_rooms.selectable.selected) {
     border: 1px solid var(--color-emerald-500);
     box-shadow: var(--shadow-2xl);
 }
+
 </style>
