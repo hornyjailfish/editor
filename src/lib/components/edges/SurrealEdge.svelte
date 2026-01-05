1e77353ebc867ef7815dbda5e4a8b54d3f4fa359
unknown <insight.outside@disroot.org>
2026-01-05 04:29:30 +0300

    basic edge

---
+++ src/lib/components/edges/SurrealEdge.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { BaseEdge, getStraightPath, type EdgeProps } from "@xyflow/svelte";
+
+let { id, data, sourceX, sourceY, targetX, targetY, ...rest } = {} as EdgeProps;
+
+let [path, labelX, labelY] = $derived(
+    getStraightPath({
+      sourceX,
+      sourceY,
+      targetX,
+      targetY,
+    })
+  );
+</script>
+
+<div class="surreal-edge">
+    <BaseEdge {id} {path} {labelX} {labelY} {...rest} />
+</div>
